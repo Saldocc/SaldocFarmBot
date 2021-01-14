414b5d4406b406d1a5beb6b97555a6876f9e181e
saldocc <onurcoban1109@gmail.com>
2021-01-14 12:21:17 +0300

    add logo to index

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,19 @@
 import React, { useState } from 'react'
 import Card from 'react-bootstrap/Card'
 import Link from 'next/link';
-
+import Image from 'next/image'
+import companyLogo from './../assets/images/logo.svg';
 function Index() {
 
   return <div>
     <Card>
       <Card.Body className="center flex-column">
+      <Image
+            alt="logo"
+            src={companyLogo}
+            width={150}
+            height={40}
+          />
         <h2 className="text-center mb-4">Ana Sayfa</h2>
         <div>
           <Link href="/login" ><a className="mr-3">Log In</a></Link>
